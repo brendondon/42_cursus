623afafdd7e7173ed059201abef5059fa5d4c4d6
brendondon <brendonsilva.santos@gmail.com>
2023-10-17 00:54:33 +0100

    ft_printf

---
+++ print/ft_printf.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/09 09:33:18 by marvin            #+#    #+#             */
+/*   Updated: 2023/10/09 09:33:18 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <stdio.h>
+
+typedef struct s_dados
+{
+	int				count;
+	int				nbase;
+	char			*base;
+	char			flag;
+	long long		nbr;
+
+}	t_dados;
+
+
+int ft_printf(const char *, ...);
+
+#endif
