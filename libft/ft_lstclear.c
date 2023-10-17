15f30c49cda0b08e84d156cd1dee5b4c6920c75e
brendondon <brendonsilva.santos@gmail.com>
2023-10-17 20:39:40 +0100

    repair ft_lstmap

--- libft/ft_lstclear.c
+++ libft/ft_lstclear.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstclear.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: breda-si <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 19:50:10 by marvin            #+#    #+#             */
-/*   Updated: 2023/10/08 19:50:10 by marvin           ###   ########.fr       */
+/*   Updated: 2023/10/17 19:49:00 by breda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 
 void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
-	t_list	*next;
+	t_list	*nlist;
 	t_list	*actual;
 
 	actual = *lst;
 	while (actual)
 	{
-		next = actual->next;
+		nlist = actual->next;
 		ft_lstdelone(actual, del);
-		actual = next;
+		actual = nlist;
 	}
 	*lst = NULL;
 }
--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstmap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: breda-si <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 21:24:53 by marvin            #+#    #+#             */
-/*   Updated: 2023/10/08 21:24:53 by marvin           ###   ########.fr       */
+/*   Updated: 2023/10/17 20:36:44 by breda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,11 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*actual;
 
 	nlst = NULL;
+	if (!lst || !f || !del)
+		return (NULL);
 	while (lst)
 	{
-		actual = ft_lstnew((*f)(lst->content));
+		actual = ft_lstnew(f(lst->content));
 		if (!actual)
 		{
 			ft_lstclear(&nlst, del);
